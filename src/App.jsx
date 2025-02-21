cb24cd339ad5fcf856159074f8594d2556f1f4ed
Laud Tetteh <kobbykurtx@gmail.com>
2025-02-21 09:15:37 +0000

    Added framer motion to the whole page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { RefsProvider } from "./contexts/RefContexts";
+import { motion } from "framer-motion";
 
+import { RefsProvider } from "./contexts/RefContexts";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
 function App() {
+  const pageVariants = {
+    hidden: {
+      opacity: 0,
+      y: 20, // Start 50px below
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "tween",
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    },
+    exit: {
+      opacity: 0,
+      y: -50, // Exit 50px above
+    },
+  };
   return (
-    <div>
-      <RefsProvider>
-        <Navbar />
+    <RefsProvider>
+      <Navbar />
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        variants={pageVariants}
+        style={{ width: "100%" }}
+      >
         <MainContent />
         <Footer />
-      </RefsProvider>
-    </div>
+      </motion.div>
+    </RefsProvider>
   );
 }
 
