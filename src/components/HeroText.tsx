ba612a1734c40a8a8cb97c49bd45392e699e4198
Laud Tetteh <kobbykurtx@gmail.com>
2025-07-26 20:35:21 +0000

    Add: Hero text animation

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -1,12 +1,51 @@
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import CtaButton from "./ctaButton";
 
+const words = ["Code", "Push", "Tech"];
+
 const HeroText = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % words.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      <div className="text-7xl font-semibold mb-4">
-        <h1 className="text-[#818181]">Code that</h1>
+      <div className="text-6xl md:text-7xl font-semibold mb-6 md:mb-4 flex flex-col">
+        <div className="flex items-baseline">
+          <div className="w-39 md:w-47 h-20 flex items-center justify-start">
+            {" "}
+            {/* Wider container */}
+            <AnimatePresence mode="wait">
+              <motion.span
+                key={words[index]}
+                initial={{ y: 40, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -40, opacity: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 80,
+                  damping: 18,
+                  mass: 1.5,
+                  opacity: { duration: 0.8 },
+                }}
+                className="text-[#818181]"
+                style={{ display: "inline-block" }}
+              >
+                {words[index]}
+              </motion.span>
+            </AnimatePresence>
+          </div>
+          <h1 className="text-[#818181]">that</h1>
+        </div>
         <h1>delivers results.</h1>
       </div>
+
       <p>
         <span className="font-semibold">
           Engineering that drives growth and performance.
